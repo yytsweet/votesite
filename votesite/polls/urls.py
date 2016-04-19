35d474c117d8383072ec3902681a1086de3314e3
cusk <yytsweet@gmail.com>
2016-04-20 04:18:57 +0800

    Tutorial03 :A shortcut render()
    Signed-off-by: cusk <yytsweet@gmail.com>

--- votesite/polls/urls.py
+++ votesite/polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.results),
 ]
--- votesite/polls/views.py
+++ votesite/polls/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+#using shortcuts for render
+#from django.template import loader
+
+from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("HW");
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    #template = loader.get_template('polls/index.html')
+    context = { 'latest_question_list': latest_question_list, }
+    #return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    #return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exit")
+    return render(request, 'polls/detail.html', {'question':question})
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
+    
+    
